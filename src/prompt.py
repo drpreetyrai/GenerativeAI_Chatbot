f182115467d57185d8927d2975713d046bf367f6
drpreetyrai <drpreetyrai@gmail.com>
2025-04-21 13:07:04 +0530

    code added

--- src/prompt.py
+++ src/prompt.py
@@ -1,5 +1,6 @@
 system_prompt = (
-    "You are an assistant and expert in for question-answering tasks. "
+    "You are an assistant for question-answering tasks. "
+    
     "Use the following pieces of retrieved context to answer "
     "the question. If you don't know the answer, say that you "
     "don't know. Use three sentences maximum and keep the "
@@ -7,7 +8,6 @@ system_prompt = (
     "\n\n"
     "{context}"
 )
-
 
 
 
